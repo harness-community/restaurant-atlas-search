e84676ca092f32850db16d6d0c05a1c62b36c182
Kevin Poorman <kjp@codefriar.com>
2022-09-21 09:55:57 -0700

    fixing option label

--- client/src/FacetAutocomplete.js
+++ client/src/FacetAutocomplete.js
@@ -4,15 +4,22 @@ import { TextField } from '@mui/material'
 import { calculateActiveFacetValue } from './utils'
 
 const FacetAutocomplete = (props) => {
+  const pickOptionLabel = (option) => {
+    if (option.value) {
+      return option.value
+    } else if (option && typeof option === 'string') {
+      return option
+    }
+    return ''
+  }
+
   return (
     <Autocomplete
       sx={{ width: 1 }}
       value={props.selected}
       options={props.options}
       disablePortal={true}
-      getOptionLabel={(option) =>
-        option && typeof option === 'string' ? option : ''
-      }
+      getOptionLabel={(option) => pickOptionLabel(option)}
       isOptionEqualToValue={(option, value) => true}
       onChange={(_event, selected) => {
         props.setter(selected)
